50e55df870562c89e7d82962d4359c54a5f9d37d
Timothy Lefkowitz <timothylefkowitz@Timothys-Air.satx.rr.com>
2025-01-31 00:44:23 -0600

    Feat: rust twoSums

---
+++ src/twoSum.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+
+fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut map = HashMap::new();
+
+    for (i, &num) in nums.iter().enumerate() {
+        let complement = target - num;
+        if let Some(&index) = map.get(&complement) {
+            return vec![index, i as i32, i as i32];
+        }
+        map.insert(num, i as i32);
+    }
+    vec![] // no solution found
+}
+
+fn main(){
+    let nums = vec![2,3,11,15];
+    let target = 9;
+    let result = two_sum(nums, target);
+
+    println!("{:?}", result);
+}
